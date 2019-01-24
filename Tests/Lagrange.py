7aa5437e9b3b85ed69bf22783ad439e41eb1cffc
matthew <matthew@natalus.net>
2019-01-24 16:18:39 -0500

    lang coef and eval

--- Tests/Lagrange.py
+++ Tests/Lagrange.py
@@ -1,24 +1,41 @@
+import pdb
 # Calculate coefficients of Lagrange Functions
 def lagrange_coef(x,y):
 	d = []
+	c = [0]*len(x)
 	for k in range(len(x)):
 		d.append(1)
 		for i in range(len(x)):
 			if i != k:
 				d[k] = d[k]*(x[k]-x[i])
-			c.append(y[k]/d[k])
+			c[k] = (y[k]/d[k])
+	return c
 
 # Evaluate Lagrange interpolation polynomial at x=t
-def langrange_Eval(t,x,c):
+def lagrange_eval(t,x,c):
 	p = []
-	N = []
+	
 	for i in range(len(t)):
 		p.append(0)
-		for j in range(len(t)):
-			N.append(1)
-			for k in range(len(t)):
-			if j != k:
-				N[j] = N[j]*(t[i] - x[k]);
-			p[i] = p[i]+N[j]*c[j]
+		N = []
+		N = [1]*len(x)
+		for j in range(len(x)):
+			#pdb.set_trace()
+			for k in range(len(x)):
+				if j != k:
+					print N[j]
+					N[j] = float(N[j])*(float(t[i]) - float(x[k]))
+			p[i] = float(p[i])+float(N[j])*float(c[j])
+	return p
 
 
+#x=[0, .5, 1, 1.5, 2]
+#y=[0, .19, .26, .29, .31]
+
+x=[0, .1, .4, .5, .6, .9, 1, 1.1, 1.4, 1.5, 1.6, 1.9, 2]
+y=[0, .06, .17, .19, .21, .25, .26, .27, .29, .29, .3, .31, .31]
+
+xi = [i/100.0 for i in range(0, 201, 1)]
+cia=lagrange_coef(x,y)
+yia=lagrange_eval(xi,x,cia)
+print yia
