4d3aacd01f5371a09c3d8ec29aa0b3811a44994c
Matthew <matthew@natalus.net>
2019-01-24 12:18:08 -0500

    functions

---
+++ Functions/simp.py
@@ -0,0 +1,4 @@
+# find integral of f using compozite Simpson rule
+# n must be even
+def simp(f,a,b,n):
+	pass
---
+++ Functions/trap.py
@@ -0,0 +1,3 @@
+# find integral of f using compozite trapezoid rule
+def trap(f,a,b,n):
+	pass
