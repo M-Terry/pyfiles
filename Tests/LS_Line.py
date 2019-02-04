3ab386a7ba63a63b70983f6da7b3636dd9bb9f0b
jfaunda3 <46972659+jfaunda3@users.noreply.github.com>
2019-02-03 23:14:24 -0500

    Update LS_Line.py
    added example

--- Tests/LS_Line.py
+++ Tests/LS_Line.py
@@ -20,5 +20,24 @@ def LS_Line(x,y):
     print '[x  y  (a*x+b)  (y-(a*x+b))'
     print table
     err = sum(table[3][:]**2)
-    s = [a,b]
+       s = [a,b] #a is slope, b is intercept
     return s
+
+""" Example """
+""" Problem 4 """
+import matplotlib.pyplot  as plt
+import numpy as np
+from Bisect import Bisect
+f = '(x**4) - 0.25'
+BisectAns = Bisect(f,0.1,1,10E-16,1000)
+root = np.sqrt(2) / 2
+error = abs(root - BisectAns[1])
+x = error[0:len(error)-1]
+y = error[1:len(error)]
+[slope,intercept]= LS_Line(x,y)
+err_k = slope * x + intercept
+fig = plt.figure()
+plt.plot(error[1:len(error)],'*',err_k)
+plt.xlabel('Iteration')
+plt.ylabel('Err_k')
+plt.title('Err_k Over Time')
